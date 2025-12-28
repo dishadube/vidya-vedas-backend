adafa8dd260aef63d79a7a6ad70f6ecdb84d1c2d
dishadube <159407792+dishadube@users.noreply.github.com>
2025-12-28 20:06:04 +0530

    Add deleteBlog route and protect middleware

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,6 +1,15 @@
 import express from "express";
-import { createBlog, getBlog } from "../controllers/blogController.js";
+import {
+  createBlog,
+  getBlog,
+  deleteBlog,
+} from "../controllers/blogController.js";
+import { protect } from "../middleware/auth.js";
+
 const router = express.Router();
-router.post("/create", createBlog);
+
+router.post("/create", protect, createBlog);
 router.get("/", getBlog);
+router.delete("/:id", protect, deleteBlog);
+
 export default router;
