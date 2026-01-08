e68964c86bc034023e9862dd29d1ae8531cf2adf
dishadube <159407792+dishadube@users.noreply.github.com>
2026-01-09 00:30:31 +0530

    Refactor createStudent for better error handling
    Refactored createStudent function to improve error handling and streamline student creation.

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,27 +1,20 @@
 import Student from "../models/Student.js";
 
 export const createStudent = async (req, res) => {
-  const {
-    studentName,
-    rollNo,
-    course,
-    motherName,
-    gender,
-    cast,
-    medium,
-  } = req.body;
-
-  if (!studentName || !rollNo || !course || !gender) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
-
-  // save student
+    try {
+        const {medium,studentName,rollNo,course,motherName,gender,cast } = req.body;
+        const student = await Student.create({medium,studentName,rollNo,course,motherName,gender,cast});
+        res.json({ msg: "student created successfully", student });
+    } catch (error) {
+        res.status(400).json({ msg: "Error creating student", error: error.message });
+    }
 };
 
 export const getStudents = async (req,res)=>{
    const students = await Student.find();
    res.json(students);
 }
+
 
 
 
